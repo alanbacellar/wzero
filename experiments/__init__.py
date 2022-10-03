b46b2e01524b34077c6bd63d463df80ec26a17d2
Alantlb <alanb@DESKTOP-OEULNMK.localdomain>
2022-10-03 13:32:22 -0300

    v10.0

--- experiments/__init__.py
+++
@@ -1,6 +0,0 @@
-from .experiment import *
-from .kfold import *
-from .mappings import *
-from .train_and_test import *
-from .hyperparameters import *
-from .preprocessings import *
--- metrics.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def accuracy(y, predictions):
-    return np.sum(y == np.argmax(predictions, axis=1)) / y.shape[0]
